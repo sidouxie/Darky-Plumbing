a527fc9bdf292a91bd11e4c17859b187678f8102
sidouxie <sid_ouxiee@hotmail.com>
2021-05-22 20:50:41 +0100

    design form contact

--- components/SeFooter.js
+++ components/SeFooter.js
@@ -85,7 +85,7 @@ export default function SeFooter() {
                 />
               </label>
 
-              <input type="submit" value="Envoyer" />
+              <input className="btn-form" type="submit" value="Envoyer" />
             </form>
           </div>
         </div>
